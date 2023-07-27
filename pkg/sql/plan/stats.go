036062a1c0b7004a5f782bd6453c2b7303a0acf3
badboynt1 <badboynt@126.com>
2023-07-27 15:52:39 +0800

    fix

--- pkg/sql/plan/stats.go
+++ pkg/sql/plan/stats.go
@@ -560,8 +560,8 @@ func ReCalcNodeStats(nodeID int32, builder *QueryBuilder, recursive bool, leafNo
 				Selectivity: 1,
 			}
 			if len(node.FilterList) > 0 {
-				node.Stats.Outcnt *= 0.001
-				node.Stats.Selectivity *= 0.001
+				node.Stats.Outcnt *= 0.0001
+				node.Stats.Selectivity *= 0.0001
 			}
 		} else {
 			node.Stats = &plan.Stats{
